e7a68ace9935078a39401eb4fd6f7df87a188a2b
kvishwasrujan <roohithbandi00@gmail.com>
2020-10-12 17:03:18 +0530

    Choosing Letter To Play Functionality Added

--- capgemini/tictactoe/TicTacToe.java
+++ capgemini/tictactoe/TicTacToe.java
@@ -1,5 +1,5 @@
 package com.capgemini.tictactoe;
-
+import java.util.Scanner;
 public class TicTacToe {
 	//constants
 		public static char user;
@@ -12,8 +12,28 @@ public class TicTacToe {
 					}
 					return board;
 				}
+		//choosing letter
+		/**
+		 * @return char
+		 */
+		private static char chooseLetter() {
+			Scanner sc=new Scanner(System.in);
+			System.out.println("Enter the Letter");
+			return sc.next().charAt(0);
+		}
+		/**
+		 * @param args
+		 */
 		public static void main(String[] args) {
 			System.out.println("Welcome to TicTacToe program");
 	        TicTacToe ticTacToe = new TicTacToe();
 			char[] board = ticTacToe.createBoard();
+			char user=ticTacToe.chooseLetter();
+			//assigning computer letter
+			if(user=='X') {
+				computer='O';
+			}
+			else {
+				computer='X';
+			}
 		}}
