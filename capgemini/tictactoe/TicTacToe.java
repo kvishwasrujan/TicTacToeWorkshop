e101d7b323566b236ebf72407e39e8ce8deb9d17
kvishwasrujan <roohithbandi00@gmail.com>
2020-10-12 17:10:24 +0530

    Select Location Functionality Added

--- capgemini/tictactoe/TicTacToe.java
+++ capgemini/tictactoe/TicTacToe.java
@@ -4,6 +4,8 @@ public class TicTacToe {
 	//constants
 		public static char user;
 		public static char computer;
+		public static char turn;
+		public static char board;
 		// Create board with Empty Spaces
 		private char[] createBoard() {
 			char[] board = new char[10];
@@ -30,6 +32,33 @@ public class TicTacToe {
 			System.out.println("______________");
 			System.out.println(board[7]+" | "+board[8]+" | "+board[9]);
 		}
+		// Move to desired location
+			/**
+			 * @param board
+			 * @param user
+			 * @return
+			 */
+			private static int moveLocation(char[] board,char user) {
+				Scanner sc = new Scanner(System.in);
+				Integer[] values = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+				while (true) {
+					System.out.println("Enter the number between 1 and 9");
+					int location = sc.nextInt();
+					if (Arrays.asList(values).contains(location) && isSpaceFree(board, location)) {
+						return location;
+					}
+				}
+
+			}
+
+			/**
+			 * @param board
+			 * @param location
+			 * @return
+			 */
+			private static boolean isSpaceFree(char[] board, int location) {
+				return board[location] == ' ';
+			}
 		public static void main(String[] args) {
 			System.out.println("Welcome to TicTacToe program");
 	        TicTacToe ticTacToe = new TicTacToe();
@@ -43,6 +72,6 @@ public class TicTacToe {
 				computer='X';
 			}
 			displayBoard(board);
+			moveLocation(board,user);
 		}
-
 		}
